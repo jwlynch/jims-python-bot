9776f23898a81f89e7b6b7fe97a0eb9d29f58ca6
Jim Lynch <jim@jam.sessionsnet.org>
2015-05-02 06:04:27 -0700

    add search by string

--- utils/commandtarget.py
+++ utils/commandtarget.py
@@ -11,9 +11,35 @@ class CommandTarget(object):
             )
           
         self._itsNextTarget = aCommandTarget
-        
+
     def doCommand(self, command):
+        result = None
         # pass the buck
-        
+
         if self._itsNextTarget is not None:
-            self._itsNextTarget.doCommand(command)
+            result = self._itsNextTarget.doCommand(command)
+
+        return result
+
+    def searchCommand(self, cmdString):
+        result = None
+        if not _itsNextTarget is None:
+            result = _itsNextTarget.searchCommand(cmdString)
+        
+        return result
+
+    def doCommandStr(self, cmdString):
+        result = None
+        cmd = self.searchCommand(cmdString)
+        
+        if cmd is not None:
+            result = self.doCommand(cmd)
+
+        return result
+
+# one way:
+#
+# define a hash with string command-name keys that go to command numbers,
+# and have searchCommand use that, then define doCommand as an if...elif
+# structure where each command number is tested. doCommand should return
+# None if it doesn't result in a command being executed
