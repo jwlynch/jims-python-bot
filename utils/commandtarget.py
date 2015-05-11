e4f8a261c69cfe976129bc7434fe5f30520c08c8
Jim Lynch <jim@jam.sessionsnet.org>
2015-05-11 12:19:12 -0700

    add suggestions for implementing commands for subclasses of CommandTarget
    Show the methods and data structires to override so that commands can be implemented
    in the subclasses of CommandTarget.

--- utils/commandtarget.py
+++ utils/commandtarget.py
@@ -13,6 +13,21 @@ class CommandTarget(object):
           
         self._itsNextTarget = aCommandTarget
 
+#
+# suggestion for command list for searchCommand
+#
+# first, set vars to command numbers
+#
+# cmdFoo = 1
+# cmdBar = 3
+#
+# then, make a dict where the keys are the command strings that come from the bot
+#
+# commands = {"foo": cmdFoo, "bar": cmdBar}
+#
+# then, the suggested code for searchCommand will pick them up. use the symbolic names in doCommsnd().
+#
+
     def doCommand(self, command, args):
         result = None
         # pass the buck
@@ -22,12 +37,44 @@ class CommandTarget(object):
 
         return result
 
+#
+# doCommand suggestion for override
+#
+# def doCommand(self, command, args)
+#     result = None
+#
+#     (extract from args whatever might be needed for running the command)
+#
+#     if command = self.cmdFoo:
+#         (run the command, set result to success (0) or raise exception)
+#     elif command = self.cmdBar:
+#         (run cmdBar, set result or raise excepton)
+#     else:
+#         # pass buck to superclass
+#         result = super(FooClass, self).doCommand(command, args)
+#
+
     def searchCommand(self, cmdString):
         result = None
         if not self._itsNextTarget is None:
             result = self._itsNextTarget.searchCommand(cmdString)
         
         return result
+
+#
+# searchCommand suggestion for override
+#
+# def searchCommand(self, cmdString)
+#     result = None
+#     for key in self.commands:
+#         if key == cmdString:
+#             result = self.commands[cmdString]
+#
+#     if result == None:
+#         result = super(FooClass, self).searchCommand(cmdString)
+#
+#     return result
+#
 
     def doCommandStr(self, cmdString, args):
         result = None
